d46eef92ea2294498d40a82d75da6a165782088c
hyeonah <hyeonah.hello@gmail.com>
2022-08-26 13:51:30 +0900

    #3 feat: Display form error message when input value is empty

--- src/pages/SignUpPage/index.jsx
+++ src/pages/SignUpPage/index.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 const SignUpPage = () => {
   const initialValue = {
@@ -9,6 +9,8 @@ const SignUpPage = () => {
     job: '',
   }
   const [formValues, setFormValues] = useState(initialValue)
+  const [formErrors, setFormErrors] = useState({})
+  const [isSubmit, setIsSubmit] = useState(false)
 
   const handleInputChange = (e) => {
     const { name, value } = e.target
@@ -17,7 +19,34 @@ const SignUpPage = () => {
 
   const handleSignUp = (e) => {
     e.preventDefault()
+    setFormErrors(signUpValidate(formValues))
   }
+
+  const signUpValidate = (values) => {
+    const errors = {}
+    if (!values.username) {
+      errors.username = '이름을 입력해주세요!'
+    }
+    if (!values.email) {
+      errors.email = '이메일을 입력해주세요!'
+    }
+    if (!values.password) {
+      errors.password = '비밀번호를 입력해주세요!'
+    }
+    if (!values.age) {
+      errors.age = '나이를 선택해주세요!'
+    }
+    if (!values.job) {
+      errors.job = '직업을 선택해주세요!'
+    }
+    return errors
+  }
+
+  useEffect(() => {
+    Object.keys(formErrors).length === 0
+      ? setIsSubmit(true)
+      : setIsSubmit(false)
+  }, [formErrors])
 
   return (
     <div className="container">
@@ -30,6 +59,7 @@ const SignUpPage = () => {
           value={formValues.username}
           onChange={handleInputChange}
         />
+        <p>{formErrors.username}</p>
         <input
           type="text"
           name="email"
@@ -37,6 +67,7 @@ const SignUpPage = () => {
           value={formValues.email}
           onChange={handleInputChange}
         />
+        <p>{formErrors.email}</p>
         <input
           type="text"
           name="password"
@@ -44,6 +75,7 @@ const SignUpPage = () => {
           value={formValues.password}
           onChange={handleInputChange}
         />
+        <p>{formErrors.password}</p>
         <select
           name="age"
           id="age"
@@ -60,6 +92,7 @@ const SignUpPage = () => {
           <option value="50대">50대</option>
           <option value="60대 이상">60대 이상</option>
         </select>
+        <p>{formErrors.age}</p>
         <select
           name="job"
           id="job"
@@ -75,6 +108,7 @@ const SignUpPage = () => {
           <option value="고령자">고령자</option>
           <option value="무직">무직</option>
         </select>
+        <p>{formErrors.job}</p>
         <button onClick={handleSignUp}>회원가입</button>
       </form>
     </div>
