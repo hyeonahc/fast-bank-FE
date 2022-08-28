0307f64f80f7ccbf958730772bd0af31b54e2210
Untest75 <khseok87n@gmail.com>
2022-08-28 15:39:55 +0900

    #2 feat: Create ProductOrderBar hook

---
+++ src/components/ProductOrderBar/hook.ts
@@ -0,0 +1,35 @@
+import { useCallback, useEffect, useState } from 'react';
+import { ORDER_TYPE, OrderType } from '@/constants/orderBar';
+import { Product } from '@/types/product';
+
+export const useProductOrderBar = (data: Product[] | undefined) => {
+  const [orderedData, setOrderedData] = useState<Product[]>([]);
+
+  const [order, setOrder] = useState<OrderType>(ORDER_TYPE.NONE);
+  const onChangeOrder = useCallback((order: OrderType) => {
+    setOrder(order);
+  }, []);
+
+  useEffect(() => {
+    if (!data) {
+      setOrderedData([]);
+      return;
+    }
+
+    switch (order) {
+      case ORDER_TYPE.NONE:
+        setOrderedData(data);
+        return;
+      case ORDER_TYPE.NAME:
+        setOrderedData([...data].sort((a, b) => a.name.localeCompare(b.name)));
+        return;
+      case ORDER_TYPE.CATALOG:
+        setOrderedData(
+          [...data].sort((a, b) => a.category.localeCompare(b.category)),
+        );
+        return;
+    }
+  }, [order, data]);
+
+  return { orderedData, onChangeOrder };
+};
