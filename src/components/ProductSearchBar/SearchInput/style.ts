32ac8b9b93eb3687263b5cb5ef36b804cfebd258
Untest75 <khseok87n@gmail.com>
2022-08-28 15:34:08 +0900

    #2 feat: Create SearchInput

---
+++ src/components/ProductSearchBar/SearchInput/style.ts
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+import Input from '@/components/Input';
+
+type Props = JSX.IntrinsicElements['input'];
+
+export const SearchInput = styled(Input).attrs<Props>({
+  className: '',
+})``;
