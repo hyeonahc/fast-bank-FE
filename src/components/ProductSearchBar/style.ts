14e7ff1c472f8e33cb590884339558ca86bce8c1
Untest75 <khseok87n@gmail.com>
2022-08-28 15:35:29 +0900

    #2 feat: Create ProductSearchBar
    - 검색 쿼리를 내부에서 처리
    - convertRTKQueryErrorToError 추가

---
+++ src/components/ProductSearchBar/style.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const Container = styled.div({
+  display: 'flex',
+  flexDirection: 'column',
+});
+
+export const FilterContainer = styled.div({
+  display: 'flex',
+  flexDirection: 'row',
+});
