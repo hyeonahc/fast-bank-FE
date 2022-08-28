dc91420a3166b3a7d9339c82d9388f9cdd0c7c57
Untest75 <khseok87n@gmail.com>
2022-08-28 15:30:42 +0900

    #2 feat: Create search API with RTK Query

--- src/modules/index.tsx
+++ src/modules/index.tsx
@@ -1,9 +1,15 @@
 import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import { setupListeners } from '@reduxjs/toolkit/query';
+import { searchApi } from '@/api/searchApi';
 
 const rootReducer = combineReducers({});
 
 const store = configureStore({
-  reducer: rootReducer,
+  reducer: { ...rootReducer, [searchApi.reducerPath]: searchApi.reducer },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(searchApi.middleware),
 });
+
+setupListeners(store.dispatch);
 
 export default store;
