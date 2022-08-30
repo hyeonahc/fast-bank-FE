97c705a6ac6a537e2affe615bd6dcb0e4f7ba464
hyeonah <hyeonah.hello@gmail.com>
2022-08-30 11:12:19 +0900

    #3 feat: Move to SignInPage after clicking login button on SuccessModal

--- src/components/SuccessModal/index.jsx
+++ src/components/SuccessModal/index.jsx
@@ -1,10 +1,18 @@
+import { useNavigate } from 'react-router-dom'
 import * as S from './style'
 
 const SuccessModal = ({ title, buttonText, displayModal, setDisplayModal }) => {
+  const navigate = useNavigate()
+
+  const onClickHandler = () => {
+    setDisplayModal(true)
+    navigate('/signin')
+  }
+
   return (
     <S.SuccessModal style={{ display: displayModal ? 'flex' : 'none' }}>
       <h2>{title}</h2>
-      <button onClick={() => setDisplayModal(true)}>{buttonText}</button>
+      <button onClick={onClickHandler}>{buttonText}</button>
     </S.SuccessModal>
   )
 }
