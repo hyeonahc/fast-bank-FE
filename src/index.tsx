33ebec31b3f8619d67d6f986f144519a8a944fdd
hyeonah <hyeonah.hello@gmail.com>
2022-08-30 12:51:52 +0900

    #14 design: Create style folder, theme.js, globalStyle.js and Add ThemeProvider to index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ThemeProvider } from 'styled-components';
+import GlobalStyle from '@/styles/globalStyle';
+import theme from '@/styles/theme';
 import './index.css';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
-root.render(<App />);
+root.render(
+  <>
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <App />
+    </ThemeProvider>
+  </>,
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
---
+++ src/styles/theme.js
@@ -0,0 +1,10 @@
+const colors = {}
+
+const common = {}
+
+const theme = {
+  colors,
+  common,
+}
+
+export default theme
