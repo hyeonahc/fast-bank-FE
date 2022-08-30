b0886ea5be273e04b0ee37bb5b59cbef5ba6ff83
Untest75 <khseok87n@gmail.com>
2022-08-30 17:22:25 +0900

    #10 feat: Add Cart RTK query

--- src/modules/index.tsx
+++ src/modules/index.tsx
@@ -1,15 +1,22 @@
 import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import { setupListeners } from '@reduxjs/toolkit/query';
+import { cartApi } from '@/api/cartApi';
 import { searchApi } from '@/api/searchApi';
 
-const rootReducer = combineReducers({});
+const rootReducer = combineReducers({
+  [cartApi.reducerPath]: cartApi.reducer,
+  [searchApi.reducerPath]: searchApi.reducer,
+});
 
 const store = configureStore({
-  reducer: { ...rootReducer, [searchApi.reducerPath]: searchApi.reducer },
+  reducer: rootReducer,
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(searchApi.middleware),
+    getDefaultMiddleware().concat(cartApi.middleware, searchApi.middleware),
 });
 
 setupListeners(store.dispatch);
 
 export default store;
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
