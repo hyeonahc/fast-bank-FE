6d374db90b46c4b05f102bb9b518b97720390fed
hyeonah <hyeonah.hello@gmail.com>
2022-09-03 19:32:58 +0900

    #14 style: Update variable name ButtonBased to Button

--- src/components/common/Button/style.js
+++ src/components/common/Button/style.js
@@ -1,6 +1,6 @@
 import styled from 'styled-components'
 
-export const ButtonBased = styled.button`
+export const Button = styled.button`
   padding: 0.5em 1.75em;
   border: 2px solid ${({ theme }) => theme.colors.primary};
   border-radius: 4px;
