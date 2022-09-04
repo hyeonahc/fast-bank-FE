870671d7452014cc1a7ccc9e275fe557aa18065b
Untest75 <khseok87n@gmail.com>
2022-09-04 18:02:03 +0900

    #53 design: Apply InputRadio

--- src/components/common/Input/InputRadio/index.tsx
+++ src/components/common/Input/InputRadio/index.tsx
@@ -7,42 +7,45 @@ const CheckSVGStyled = styled(CheckSVG).attrs(({ theme }) => ({
   fill: theme.colors.primary,
 }))({
   visibility: 'hidden',
-  marginRight: '0.2rem',
   textAlign: 'center',
+  marginRight: '0.6rem',
 });
 
-const InputRadio = styled.input.attrs({
+const InputRadioStyled = styled.input.attrs({
   type: 'radio',
-})({
+})(({ theme }) => ({
   appearance: 'none',
+  position: 'relative',
   padding: 0,
   margin: 0,
-  width: 0,
-  height: 0,
-  '&:not(:checked)::before': {
-    content: `"·"`,
-    display: 'inline-block',
-    paddingLeft: '0.5em',
-    paddingRight: '0.5em',
-    fontWeight: 700,
-    textAlign: 'center',
+  fontSize: 'inherit',
+  '&:not(:checked)': {
+    '&::before': {
+      content: `"·"`,
+      position: 'absolute',
+      display: 'block',
+      width: '1em',
+      height: '1em',
+      bottom: '-0.15em',
+      fontWeight: 700,
+      textAlign: 'center',
+    },
   },
   [`&:checked + ${CheckSVGStyled}`]: {
     visibility: 'visible',
   },
-});
+}));
 
-// text 타입만으로 막으려고 했는데 너무 많아서 일단 보류(HTMLInputTypeAttribute)
 type Props = {} & Omit<JSX.IntrinsicElements['input'], 'ref' | 'type'>;
 
 // placeholder 는 원래 지원
-const InputText = forwardRef<HTMLInputElement, Props>((props, ref) => {
+const InputRadio = forwardRef<HTMLInputElement, Props>((props, ref) => {
   return (
     <>
-      <InputRadio {...props} ref={ref} />
+      <InputRadioStyled {...props} ref={ref} />
       <CheckSVGStyled width="1em" height="1em" />
     </>
   );
 });
 
-export default InputText;
+export default InputRadio;
