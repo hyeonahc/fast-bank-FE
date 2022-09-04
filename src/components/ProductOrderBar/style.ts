17122123dd82bc572f78000ea6da8b4a4f03fa6e
Untest75 <khseok87n@gmail.com>
2022-09-04 18:02:37 +0900

    #53 design: Apply ProductOrderBar

--- src/components/ProductOrderBar/style.ts
+++ src/components/ProductOrderBar/style.ts
@@ -1,5 +1,25 @@
 import styled from 'styled-components';
+import InputRadio from '@/components/common/Input/InputRadio';
 
-export const Container = styled.div({});
+export const Container = styled.div({
+  display: 'flex',
+  flexDirection: 'row',
+  flexFlow: 'nowrap',
+  height: '2.4rem',
+  '> *': {
+    marginRight: '1rem',
+  },
+});
 
-export const Label = styled.label({});
+export const InputRadioStyled = styled(InputRadio)({});
+
+export const Label = styled.label(({ theme }) => ({}));
+
+export const LabelContainer = styled.div(({ theme }) => ({
+  fontSize: theme.fontSize.regular,
+  color: theme.colors.inputPlaceholder,
+
+  [`${InputRadioStyled}:checked ~ ${Label}`]: {
+    color: theme.colors.primary,
+  },
+}));
---
+++ src/styles/mixins.ts
@@ -0,0 +1,6 @@
+import { css } from 'styled-components';
+
+export const inputInPage = css(({ theme }) => ({
+  height: '4.4rem',
+  padding: '1rem 1.2rem',
+}));
