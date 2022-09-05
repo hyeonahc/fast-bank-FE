eb8bdb77574bffd1a4c418f701147d33ea1c080f
hyeonah <hyeonah.hello@gmail.com>
2022-09-05 17:26:00 +0900

    #82 design: Update Input style.js to align with mockup

--- src/components/common/Input/InputText/style.js
+++ src/components/common/Input/InputText/style.js
@@ -1,8 +1,17 @@
 import styled from 'styled-components'
 
 export const Input = styled.input`
-  width: 20rem;
-  padding: 0.5rem 0.35rem;
-  border: 1px solid #ddd;
-  border-radius: 5px;
+  width: 100%;
+  padding: 1rem;
+  border: 1px solid ${({ theme }) => theme.colors.inputBorder};
+  border-radius: 8px;
+  font-size: ${({ theme }) => theme.fontSize.regular};
+  color: ${({ theme }) => theme.colors.text};
+  &::placeholder {
+    color: ${({ theme }) => theme.colors.inputPlaceholder};
+  }
+  &:focus {
+    outline: none;
+    border: 1px solid ${({ theme }) => theme.colors.primary};
+  }
 `
